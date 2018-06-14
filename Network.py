922a7af6585e67fa180341455fbd5fa649023560
Thomas McOscar <tmcoscar@gmail.com>
2018-06-14 07:47:06 -0400

    Commented weights biases. Playground adjustments.

--- Network.py
+++ Network.py
@@ -12,5 +12,8 @@ class Network(object):
     def __init__(self, sizes):
         self.num_layers = len(sizes)
         self.sizes = sizes
+        #Goes from one dimmension to two. Holds biases, one for each neuron in a hidden layer and output layer
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        #Two dimmensions to three. Each hidden/output neuron gets weights from each prev generation neuron.
+        #Cycles through sizes using sizes[R] for rows and sizes[R-1=C] for columns, thus the offset.
         self.weights = [np.random.radn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
--- Playground.py
+++ Playground.py
@@ -5,4 +5,5 @@ sizes = [6,4,4,2]
 
 #bias = [numpy.random.randn(y,1) for y in sizes[1:]]
 
-print (sizes[:-2])
+print (sizes[:-1])
+print (sizes[1:])

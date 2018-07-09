89c8c1962e67387effe7cf03ba12f55c0615b5d6
Thomas McOscar <tmcoscar@gmail.com>
2018-07-09 14:09:36 -0400

    continuing to add functions for data use and comments for description. Decided on OO class structure.

--- DataManager.py
+++ DataManager.py
@@ -1,12 +1,35 @@
 import struct
 
-class DataSet(object):
+def dataSet(imagesPath = "train-images-idx3-ubyte", labelsPath = "train-labels-idx1-ubyte"):
+    imagesFile = open(imagesPath, "rb")
+    labelsFile = open(labelsPath, "rb")
 
-    def __init__(self, imagesPath = "train-images-idx3-ubyte", labelsPath = "train-labels-idx1-ubyte"):
-        self.imagesFile = open(imagesPath, "rb")
-        self.labelsFile = open(labelsPath, "rb")
-        self._pickLabels()
+    imagesList = []
+    labelsList = []
 
+    _magicNumbers(imagesFile, labelsFile)
 
-    def _pickLabels(self):
-        print(struct.unpack(">i", self.labelsFile.read(4))[0])
+    if (_checkEntries(imagesFile, labelsFile)):
+        imagesList = _pickImages(imagesFile)
+        labelsList = _pickLabels(labelsFile)
+    else:
+        print("Image/label count mismatch.")
+
+    # Close opened files just before returning important data
+    imagesFile.close()
+    labelsFile.close()
+
+    return imagesList, labelsList
+
+def _magicNumbers(images, labels): # ust prints magic numbers for each data file opened. Could very well be ignored.
+    #Files should be read here to maintain correct reading sequence of bytes, but printing could be omitted to save output
+    print ("Images Magic Number: " + struct.unpack(">i", images.read(4))[0])
+    print ("Labels Magic Number: " + struct.unpack(">i", labels.read(4))[0])
+
+def _checkEntries(images, labels): # Returns a quick boolean check to make sure there are the correct number of entries for each list.
+    imgCt = struct.unpack(">i", images.read(4))[0]
+    lblCt = struct.unpack(">i", labels.read(4))[0]
+    print ("Images Count: " + imgCt)
+    print ("Labels Count: " + lblCt)
+
+    return (imgCt == lblCt)

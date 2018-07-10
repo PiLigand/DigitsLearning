a3a5c09a3bb0926915039dab8312803780c05eca
Thomas McOscar <tmcoscar@gmail.com>
2018-07-09 20:35:20 -0400

    continuing to add functions for data use and comments for description. Decided on OO class structure.

--- DataManager.py
+++ DataManager.py
@@ -1,35 +1,51 @@
 import struct
 
-def dataSet(imagesPath = "train-images-idx3-ubyte", labelsPath = "train-labels-idx1-ubyte"):
-    imagesFile = open(imagesPath, "rb")
-    labelsFile = open(labelsPath, "rb")
+class DataSet(object):
 
-    imagesList = []
-    labelsList = []
+    def __init__(self,  imagesPath = "train-images-idx3-ubyte", labelsPath = "train-labels-idx1-ubyte"):
+        self.imagesFile = open(imagesPath, "rb")
+        self.labelsFile = open(labelsPath, "rb")
 
-    _magicNumbers(imagesFile, labelsFile)
+        _magicNumbers()
 
-    if (_checkEntries(imagesFile, labelsFile)):
-        imagesList = _pickImages(imagesFile)
-        labelsList = _pickLabels(labelsFile)
-    else:
-        print("Image/label count mismatch.")
+        if (_checkEntries()):
+            _pickImages()
+            _pickLabels()
+        else:
+            print("Image/label count mismatch.")
 
-    # Close opened files just before returning important data
-    imagesFile.close()
-    labelsFile.close()
+            # Close opened files after collecting important data
+            self.imagesFile.close()
+            self.labelsFile.close()
 
-    return imagesList, labelsList
+    def _magicNumbers(self): # ust prints magic numbers for each data file opened. Could very well be ignored.
+        #Files should be read here to maintain correct reading sequence of bytes, but printing could be omitted to save output
+        self.imgMN = struct.unpack(">i", self.imagesFile.read(4))[0]
+        self.lblMN = struct.unpack(">i", self.labelsFile.read(4))[0]
+        #Comment these lines to neglect output
+        print ("Images Magic Number: " + self.imgMN)
+        print ("Labels Magic Number: " + self.lblMN)
 
-def _magicNumbers(images, labels): # ust prints magic numbers for each data file opened. Could very well be ignored.
-    #Files should be read here to maintain correct reading sequence of bytes, but printing could be omitted to save output
-    print ("Images Magic Number: " + struct.unpack(">i", images.read(4))[0])
-    print ("Labels Magic Number: " + struct.unpack(">i", labels.read(4))[0])
+    def _checkEntries(self): # Returns a quick boolean check to make sure there are the correct number of entries for each list.
+        self.imgCt = struct.unpack(">i", self.imagesFile.read(4))[0]
+        self.lblCt = struct.unpack(">i", self.labelsFile.read(4))[0]
+        #Can comment these to neglect the output.
+        print ("Images Count: " + self.imgCt)
+        print ("Labels Count: " + self.lblCt)
 
-def _checkEntries(images, labels): # Returns a quick boolean check to make sure there are the correct number of entries for each list.
-    imgCt = struct.unpack(">i", images.read(4))[0]
-    lblCt = struct.unpack(">i", labels.read(4))[0]
-    print ("Images Count: " + imgCt)
-    print ("Labels Count: " + lblCt)
+        return (imgCt == lblCt)
 
-    return (imgCt == lblCt)
+    def _pickImages(self):
+        # For MNIST files as of 07/09/18, these values should each be 28 (integer)
+        self.imgRows = struct.unpack(">i", self.imagesFile.read(4))[0]
+        self.imgCols = struct.unpack(">i", self.imagesFile.read(4))[0]
+
+        #Initialize empty list. Each item will have a two dimmensional list within
+        self.ImagesList = []
+
+        #For loop to cycle through each item / each image
+        for i in range(0, self.imgCt):
+            self.ImagesList.append([])
+            #For loop for first dimmensions (to 28)
+                Append
+                #For loop for second dimmensions (to 28)

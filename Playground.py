494ffc111a3c6e369ed64d69401166d80a9285c0
Thomas McOscar <tmcoscar@gmail.com>
2018-07-12 12:53:14 -0400

    Altered for testing DataManager and Network.

--- Playground.py
+++ Playground.py
@@ -1,6 +1,13 @@
+# Self-built modules
 import Network
 import DataManager
 
-import Numpy
+# Third-party modules
+import numpy
+
+# Built-in modules
+import struct
 
 MNIST = DataManager.DataSet()
+
+training_data, validation_data, test_data = MNIST.NielsenTuple(50000, 10000, 10000)
--- swingSet.py
+++ swingSet.py
@@ -1,12 +1,22 @@
 import struct
+import DataManager
 
 imagesPath = "train-images-idx3-ubyte"
+labelsPath = "train-labels-idx1-ubyte"
 imagesFile = open(imagesPath, "rb")
+labelsFile = open(labelsPath, "rb")
 
-print("Magic Number: %i" % struct.unpack(">i", imagesFile.read(4))[0])
+print("Magic Number: %i" % struct.unpack(">i", labelsFile.read(4))[0])
 
-print("Number Entries: %i" % struct.unpack(">i", imagesFile.read(4))[0])
+print("Number Entries: %i" % struct.unpack(">i", labelsFile.read(4))[0])
 
-print("Images Rows: %i" % struct.unpack(">I", imagesFile.read(1))[0])
 
-print("Images Cols: %i" % struct.unpack(">I", imagesFile.read(1))[0])
+#print("Images Rows: %i" % struct.unpack(">i", imagesFile.read(4))[0])
+#print("Images Cols: %i" % struct.unpack(">i", imagesFile.read(4))[0])
+
+for i in range(0, 784): #Should be first image in set
+    print("Label %i: %i" % (i, struct.unpack(">B", labelsFile.read(1))[0]))
+
+
+imagesFile.close()
+labelsFile.close()

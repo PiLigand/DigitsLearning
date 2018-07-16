0bf1e54b694db0806f16b7b80c3ea1a693f6ec55
Thomas McOscar <tmcoscar@gmail.com>
2018-07-16 08:40:09 -0400

    Adjusted to run Network

--- Playground.py
+++ Playground.py
@@ -11,5 +11,5 @@ import struct
 MNIST = DataManager.DataSet()
 training_data, validation_data, test_data = MNIST.NielsenTuple(50000, 10000, 10000)
 
-net = Network.Network([784, 30, 10])
+net = Network.Network([784, 30, 30, 10])
 net.SGD(training_data, 30, 10, 3.0, test_data)
